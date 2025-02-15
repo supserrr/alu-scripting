5ac3ff06acd0086086c23b1fa66c4ddf510ddcbd
supserrr <s.shima@alustudent.com>
2025-02-15 14:42:29 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
+function that queries the 'Reddit API'
+and prints the titles of the first 10 hot posts listed for a given subreddit.
 """
 import requests
 
