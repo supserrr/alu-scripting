dec0a4b92b602ec085c89d22eccafeb81b6750b9
supserrr <s.shima@alustudent.com>
2025-02-18 09:51:22 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,22 +1,47 @@
 #!/usr/bin/python3
-"""
-function that queries the 'Reddit API'
-and prints the titles of the first 10 hot posts listed for a given subreddit.
-"""
+'''
+A module containing functions for working with the Reddit API.
+The module retrieves the titles of the top 10 posts from a given subreddit.
+'''
+
 import requests
+
+BASE_URL = 'https://www.reddit.com'
+'''Reddit's base API URL.'''
+
 def top_ten(subreddit):
-    """Fetch and print the top 10 hot posts from a subreddit."""
-    reddit_url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-agent': 'Mozilla/5.0'}
-    response = requests.get(reddit_url, headers=headers, params={'limit': 10})
-    # Check if the subreddit exists and the request was successful
-    if response.status_code == 200:
-        data = response.json().get('data', {})
-        posts = data.get('children', [])
-        if posts:
-            for post in posts:
+    '''Retrieves the title of the top ten posts from a given subreddit.'''
+    api_headers = {
+        'Accept': 'application/json',
+        'User-Agent': ' '.join([
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+            'AppleWebKit/537.36 (KHTML, like Gecko)',
+            'Chrome/97.0.4692.71',
+            'Safari/537.36',
+            'Edg/97.0.1072.62'
+        ])
+    }
+    sort = 'top'
+    limit = 10
+    res = requests.get(
+        '{}/r/{}/.json?sort={}&limit={}'.format(
+            BASE_URL,
+            subreddit,
+            sort,
+            limit
+        ),
+        headers=api_headers,
+        allow_redirects=False
+    )
+
+    # If the status code is 200, the subreddit exists
+    if res.status_code == 200:
+        try:
+            # Print the titles of the top 10 posts
+            for post in res.json()['data']['children'][0:10]:
                 print(post['data']['title'])
-        else:
-            print("OK")  # Subreddit exists but has no posts
+        except KeyError:
+            print(None)
     else:
-        print("OK")  # Subreddit does not exist or other error
+
+        print("OK") 
