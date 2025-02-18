6b246ab0bc823a7c22bd53623b54529be55704f2
supserrr <s.shima@alustudent.com>
2025-02-18 09:44:18 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -10,11 +10,10 @@ def top_ten(subreddit):
     """ prints the titles of the first 10 hot posts listed in a subreddit """
     url = 'https://oauth.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
-    if response.status_code == 200:
-        json = response.json()
-        data = json["data"]["children"]
-        for post in data:
-            print(post.get("data", {}).get("title"))
-    else:
-        print("None")
-# top_ten("sudan")
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        print(None)
+        return
+    posts = response.json()['data']['children']
+    for post in posts:
+        print(post['data']['title'])
