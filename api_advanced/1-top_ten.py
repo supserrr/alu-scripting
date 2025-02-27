6ce5b9b7bde9d49ac0f3a664b6c684d7c4c3c19a
supserrr <s.shima@alustudent.com>
2025-02-27 18:50:30 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,24 +4,15 @@ import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts listed in a subreddit"""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {"User-Agent": "my_bot/1.0"}
-
+    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'Chrome/1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-
-    # Check if the subreddit exists and API response is valid
+    # print(response)
+    print(response.text[189097:])
     if response.status_code != 200:
         print(None)
-        return
-
-    try:
-        posts = response.json().get("data", {}).get("children", [])
-        if not posts:
-            print(None)
-            return
-
+    else:
+        posts = response.json().get('data').get('children')
         for post in posts:
-            print(post["data"]["title"])
-    except Exception:
-        print(None)
+            print(post['data']['title'])
